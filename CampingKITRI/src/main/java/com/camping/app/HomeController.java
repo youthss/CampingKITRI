199e66e88993e893082e5da7b29053bc6116ccd6
alallallale <53548126+alallallale@users.noreply.github.com>
2020-02-05 17:53:25 +0900

    utf-8로!!

--- CampingKITRI/src/main/java/com/camping/app/HomeController.java
+++ CampingKITRI/src/main/java/com/camping/app/HomeController.java
@@ -31,12 +31,9 @@ public class HomeController {
 
 		String formattedDate = dateFormat.format(date);
 
-		
-		model.addAttribute("serverTime", formattedDate );
-		System.out.println("��������222");
-		System.out.println("��������333");
-		
-		
+		model.addAttribute("serverTime", formattedDate);
+		System.out.println("UTF-8로 변경해요");
+
 		return "home";
 	}
 
