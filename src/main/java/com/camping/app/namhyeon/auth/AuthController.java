545c968dde8a792f849ddf02d3b9390d10355647
alallallale <53548126+alallallale@users.noreply.github.com>
2020-02-05 18:02:55 +0900

    앞으로 꺼냄

--- src/main/java/com/camping/app/namhyeon/auth/AuthController.java
+++ src/main/java/com/camping/app/namhyeon/auth/AuthController.java
@@ -9,8 +9,11 @@ import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.camping.app.service.MemberService;
 
